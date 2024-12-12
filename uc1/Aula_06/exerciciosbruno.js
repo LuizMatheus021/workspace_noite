2d23c82666d94565f81b17cfcb2d5db7f6917041
Luiz Matheus Angelo <angeloluizmatheus@gmail.com>
2024-12-12 18:36:01 -0300

    Update exerciciosbruno.js

--- uc1/Aula_06/exerciciosbruno.js
+++ uc1/Aula_06/exerciciosbruno.js
@@ -1,10 +1,10 @@
 let desempenhoDoEmpregado = prompt(parseInt = "Digite o numero de vendas:");
 if(desempenhoDoEmpregado >= 0 && desempenhoDoEmpregado < 4){
-    document.write("Desempenho Insuficiente ")
+    document.write("Desempenho Insuficiente ");
 }else if(desempenhoDoEmpregado >= 5 && desempenhoDoEmpregado <= 9){
-    document.write("Desempenho Satisfatório")
+    document.write("Desempenho Satisfatório");
 }else if (desempenhoDoEmpregado >= 10 ){
-    document.write("Desempenho Excelente")
+    document.write("Desempenho Excelente");
 }else{
     document.write("Numero de vendas Inválida");
 }
